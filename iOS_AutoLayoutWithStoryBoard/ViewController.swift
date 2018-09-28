35be38307afd253618b030c35a074a72719f31c9
DaaasukeMurata <daaaaaasuke@gmail.com>
2018-09-28 23:55:46 +0900

    [add] basic function

--- iOS_AutoLayoutWithStoryBoard/ViewController.swift
+++ iOS_AutoLayoutWithStoryBoard/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBAction func touchDownButton(_ sender: Any) {
+        label.text = "Don't touch me!!"
+        label.textColor = UIColor.red
+        label.font = UIFont.systemFont(ofSize: 40)
+    }
+    @IBAction func touchUpButton(_ sender: Any) {
+        label.text = "No touch"
+        label.textColor = UIColor.black
+        label.font = UIFont.systemFont(ofSize: 10)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
