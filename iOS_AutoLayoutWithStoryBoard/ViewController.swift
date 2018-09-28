fd73a098bd7c626a02533448f6a0e3f3bcb7fb17
DaaasukeMurata <daaaaaasuke@gmail.com>
2018-09-28 22:39:35 +0900

    Initial Commit

---
+++ iOS_AutoLayoutWithStoryBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_AutoLayoutWithStoryBoard
+//
+//  Created by MURATA DAISUKE on 2018/09/28.
+//  Copyright © 2018 MURATA DAISUKE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
